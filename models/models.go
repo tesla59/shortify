2eef4e1c6d2535f28468985bfd7048d13fc52c2c
tesla59 <nishant@heim.id>
2023-06-19 15:05:06 +0530

    models: define json, gorm field tags

--- models/models.go
+++ models/models.go
@@ -1,9 +1,9 @@
 package models
 
 type URL struct {
-	ID      uint64
-	Target  string
-	URL     string
-	Clicked uint64
-	Random  bool
+	ID      uint64 `json:"id" gorm:"primaryKey"`
+	Target  string `json:"target" gorm:"not null"`
+	URL     string `json:"url" gorm:"not null"`
+	Clicked uint64 `json:"clicked"`
+	Random  bool   `json:"random"`
 }
