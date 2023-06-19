b393d37191897f65e1e9761c60526dae6ebee5a7
tesla59 <nishant@heim.id>
2023-06-19 12:40:15 +0530

    database: update db functions

--- database/database.go
+++ database/database.go
@@ -20,3 +20,42 @@ func InitDB() {
 	DBConn.AutoMigrate(&models.URL{})
 	fmt.Println("Migration Successful")
 }
+
+func GetAllURLs() ([]models.URL, error) {
+	var URLs []models.URL
+	tx := DBConn.Find(&URLs)
+	if tx.Error != nil {
+		return []models.URL{}, tx.Error
+	}
+	return URLs, nil
+}
+
+func GetURL(id uint64) (models.URL, error) {
+	var URL models.URL
+	tx := DBConn.First(&URL, id)
+	if tx.Error != nil {
+		return URL, nil
+	}
+	return URL, nil
+}
+
+func CreateURL(URL models.URL) error {
+	return DBConn.Create(&URL).Error
+}
+
+func UpdateURL(URL models.URL) error {
+	return DBConn.Save(&URL).Error
+}
+
+func DeleteURL(id uint64) error {
+	return DBConn.Unscoped().Delete(&models.URL{}, id).Error
+}
+
+func FindURLbyTarget(target string) (models.URL, error) {
+	var URL models.URL
+	tx := DBConn.Where("target = ?", target).First(&URL)
+	if tx.Error != nil {
+		return models.URL{}, tx.Error
+	}
+	return URL, nil
+}
