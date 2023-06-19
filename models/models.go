5dfa94b1301adad5ce80d5302b37b1eef230091c
tesla59 <nishant@heim.id>
2023-06-19 16:40:22 +0530

    models: drop json tag for Clicked in URL struct

--- models/models.go
+++ models/models.go
@@ -4,6 +4,6 @@ type URL struct {
 	ID        uint64 `json:"id" gorm:"primaryKey"`
 	TargetURL string `json:"target_url" gorm:"not null"`
 	ShortURL  string `json:"short_url" gorm:"unique;not null"`
-	Clicked   uint64 `json:"clicked"`
+	Clicked   uint64
 	Random    bool   `json:"random"`
 }
